2dd3dea266009ecbe1729712b99a7b513af177e5
delton <hulbert.delton@gmail.com>
2025-03-23 17:58:22 -0400

    moving utility functions out of UI utils and into data utils, fixing class keywords

--- Assets/Scripts/Utils/UiUtils.cs
+++ Assets/Scripts/Utils/UiUtils.cs
@@ -512,36 +512,5 @@ namespace SMercenaries.Tooling
             }
         GUI.enabled = true;
         }
-
-        /// <summary>
-        /// Attempts to retrieve a particular Monobehaviour Component from a GameObject, and if it is not present, adds it to the object.
-        /// </summary>
-        /// <typeparam name="T">the type of the component to retrieve</typeparam>
-        /// <param name="go">the GameObject to search for the component on</param>
-        /// <returns>The found or created component</returns>
-        public static T GetOrAddComponent<T>(this GameObject go) where T : Component
-        {
-            var comp = go.GetComponent<T>();
-            return comp != null ? comp : go.AddComponent<T>();
-        }
-
-#if UNITY_EDITOR
-        /// <summary>
-        /// Searches through the Asset Database for assets of a specific type and loads them into memory
-        /// </summary>
-        /// <typeparam name="T">the type of objects to search for in the Asset Database</typeparam>
-        /// <returns>Array of objects (T[]) that have been loaded</returns>
-        public static T[] LoadAssetsOfType<T>() where T : UnityEngine.Object
-        {
-            var assetsToLoad = AssetDatabase.FindAssets($"t:{typeof(T)}");
-            T[] values = new T[assetsToLoad.Length];
-            for (int i = 0; i < assetsToLoad.Length; i++)
-            {
-                var guidToPath = AssetDatabase.GUIDToAssetPath(assetsToLoad[i]);
-                values[i] = AssetDatabase.LoadAssetAtPath<T>(guidToPath);
-            }
-            return values;
-        }
-#endif
     }
 }
