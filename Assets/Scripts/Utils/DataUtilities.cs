9e77c2463d9c195c9c8e3a6f39986885ec028668
KIRRYBIRRY\noahm <noah.miannan@gmail.com>
2025-04-02 18:48:29 -0400

    Added If Unity_editor to DataUtilities, UnityEditor not available for playable builds. Will Need to find a new way to search for scriptable objects.

--- Assets/Scripts/Utils/DataUtilities.cs
+++ Assets/Scripts/Utils/DataUtilities.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
+using Unity.VisualScripting;
 
 namespace SMercenaries.Tooling
 {
@@ -10,28 +13,31 @@ namespace SMercenaries.Tooling
         /// </summary>
         /// <typeparam name="T">the type of objects to search for in the Asset Database</typeparam>
         /// <returns>Array of objects (T[]) that have been loaded</returns>
-        public static T[] LoadAssetsOfType<T>() where T : UnityEngine.Object
-        {
-            var assetsToLoad = AssetDatabase.FindAssets($"t:{typeof(T)}");
-            T[] values = new T[assetsToLoad.Length];
-            for (int i = 0; i < assetsToLoad.Length; i++)
-            {
-                var guidToPath = AssetDatabase.GUIDToAssetPath(assetsToLoad[i]);
-                values[i] = AssetDatabase.LoadAssetAtPath<T>(guidToPath);
-            }
-            return values;
-        }
 
-        /// <summary>
-        /// Attempts to retrieve a particular Monobehaviour Component from a GameObject, and if it is not present, adds it to the object.
-        /// </summary>
-        /// <typeparam name="T">the type of the component to retrieve</typeparam>
-        /// <param name="go">the GameObject to search for the component on</param>
-        /// <returns>The found or created component</returns>
-        public static T GetOrAddComponent<T>(this GameObject go) where T : Component
-        {
-            var comp = go.GetComponent<T>();
-            return comp != null ? comp : go.AddComponent<T>();
-        }
+        #if UNITY_EDITOR
+        public static T[] LoadAssetsOfType<T>() where T : UnityEngine.Object
+            {
+                var assetsToLoad = AssetDatabase.FindAssets($"t:{typeof(T)}");
+                T[] values = new T[assetsToLoad.Length];
+                for (int i = 0; i < assetsToLoad.Length; i++)
+                {
+                    var guidToPath = AssetDatabase.GUIDToAssetPath(assetsToLoad[i]);
+                    values[i] = AssetDatabase.LoadAssetAtPath<T>(guidToPath);
+                }
+                return values;
+            }
+
+            /// <summary>
+            /// Attempts to retrieve a particular Monobehaviour Component from a GameObject, and if it is not present, adds it to the object.
+            /// </summary>
+            /// <typeparam name="T">the type of the component to retrieve</typeparam>
+            /// <param name="go">the GameObject to search for the component on</param>
+            /// <returns>The found or created component</returns>
+            public static T GetOrAddComponent<T>(this GameObject go) where T : Component
+            {
+                var comp = go.GetComponent<T>();
+                return comp != null ? comp : go.AddComponent<T>();
+            }
+        #endif
     }
 }
